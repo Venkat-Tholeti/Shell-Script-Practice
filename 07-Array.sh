e2cedf6e94db40868015f38381a235ef4fdc74d2
venkat <devopsaws84s@gmail.com>
2025-10-31 08:54:12 +0530

    SHELL SCRITING PRACTICE

--- 07-Array.sh
+++ 07-Array.sh
@@ -6,11 +6,11 @@ places=("kerala" "Andhra" "Telangana" "Bangalore" "Madhurai")
 echo i want to visit ${places[0]} this year
 
 echo i want to visit all the below locations 
-${places[%]}
+${places[@]}
 
 #using arguments just for practice
 
-places=$1
+places=$1s
 
 
 echo i will not visit ${places[$1]} in 2026 # This will not work with index 0 that means kerala wont be shown in ouput
