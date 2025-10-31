b52f5920b454bbe886b2aacb02eea72a73f5f1af
venkat <devopsaws84s@gmail.com>
2025-10-31 08:43:05 +0530

    SHELL SCRITING PRACTICE

--- 07-Array.sh
+++ 07-Array.sh
@@ -8,6 +8,5 @@ echo i want to visit ${places[3]} this year
 #using arguments just for practice
 
 places=$1
-places=("kerala" "Andhra" "Telangana" "Bangalore" "Madhurai")
 
 echo i dont want to visit ${places[$1]} this year
