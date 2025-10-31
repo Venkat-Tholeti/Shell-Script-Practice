10c1dd61f5f78678f53c58656fbeb32e40a7c979
venkat <devopsaws84s@gmail.com>
2025-10-31 07:03:20 +0530

    HELLO WORLD

---
+++ 03-Variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+Person1=Sita
+Person2=Gita
+Person3=Rita
+
+echo "$Person1: Hi $Person2, how are you"
+echo "$Person2: Fine!, How about you $Person1?"
+echo "$Person3: Hi buddies ! $Person1 & $Person2"
+echo  "$Person1 : Hi $Person3!"
+echo  "$Person2 : Hi $Person3!"
