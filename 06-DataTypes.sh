2360934f8a430ed99ef1fc1d47d86746d0ab8caf
venkat <devopsaws84s@gmail.com>
2025-10-31 08:03:54 +0530

    SHELL SCRITING PRACTICE

--- 06-DataTypes.sh
+++ 06-DataTypes.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+
 number1=$1
 number2=$2
+number3=40
+number4=50
 
 Timestamp=$(date)
 
 Total=$(($number1+$number2))
+Total=$(($number3-$number4))
 
 echo Addition of $number1 and $number2 is $Total
+echo Subtraction of $number3 and $number4 is $Total
 
 echo This script ran on $Timestamp
 
