24d5b05f41d1941c1d8279c9fae5e37843306224
venkat <devopsaws84s@gmail.com>
2025-10-31 07:07:26 +0530

    HELLO WORLD

---
+++ 04-Arguments.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+Person1=$1
+Person2=$2
+Person3=$3
+
+echo "$Person1: Hi $Person2, how are you"
+echo "$Person2: Fine!, How about you $Person1?"
+echo "$Person3: Hi buddies ! $Person1 & $Person2"
+echo  "$Person1 : Hi $Person3!"
+echo  "$Person2 : Hi $Person3!"
