155c4024fe44fac83a66790186f5dcbe2cb2aa4f
venkat <devopsaws84s@gmail.com>
2025-10-31 08:48:19 +0530

    SHELL SCRITING PRACTICE

--- 07-Array.sh
+++ 07-Array.sh
@@ -8,7 +8,7 @@ echo i want to visit ${places[3]} this year
 #using arguments just for practice
 
 places=$1
-places=$2
 
-echo i will visit ${places[$1]} in 2026 and wont  visit ${places[$2]} this year
+
+echo i will not visit ${places[$1]} in 2026 
 
