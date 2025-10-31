5a1221071643955eb5f3c8d9a2301412cf0747dd
venkat <devopsaws84s@gmail.com>
2025-10-31 07:52:28 +0530

    SHELL SCRITING PRACTICE

--- 06-DataTypes.sh
+++ 06-DataTypes.sh
@@ -7,7 +7,7 @@ Timestamp=$(date)
 
 Total=$(($number1+$number2))
 
-echo addition of $number1 and $number2 is $Total
+echo Addition of $number1 and $number2 is $Total
 
-echo This script is ran on $Timestamp
+echo This script ran on $Timestamp
 
