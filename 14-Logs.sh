df68eb8c320ad4ed68d9dd406953ffc7a250c537
venkat <devopsaws84s@gmail.com>
2025-12-09 15:27:26 +0530

    logs

--- 14-Logs.sh
+++ 14-Logs.sh
@@ -12,7 +12,7 @@ SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
-echo "script started executing at $(date) 
+echo "script started executing at $(date)"
 
 
 USERID=$(id -u)
