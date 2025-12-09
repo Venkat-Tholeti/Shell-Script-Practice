5d3e081ae3835024a9b76958a2f1a44ea796eaea
venkat <devopsaws84s@gmail.com>
2025-12-09 08:13:57 +0530

    special vars

--- 08-SpecialVars.sh
+++ 08-SpecialVars.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 echo "All variables passed to the script: $@"
 echo "Number of variables passed: $#"
+echo "script name: $0"
+echo "present working diredctory is $PWD"
+echo "user running the script is $USER"
+echo "home directory is $HOME"
+echo "PID of the script is $$"
+sleep 10 &
+echo "PID of the last command ranin background is $!"
