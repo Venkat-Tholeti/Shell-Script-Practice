1c78e9a2556166e1c92fd3e59365b679b7affd02
venkat <devopsaws84s@gmail.com>
2025-12-09 16:17:55 +0530

    loop

--- 16-Loops.sh
+++ 16-Loops.sh
@@ -14,9 +14,8 @@ PACKAGES=("mysql" "python3" "nginx" "httpd")
 
 mkdir -p $LOGS_FOLDER
 echo -e "$G script started executing at $(date)" &>>$LOG_FILE
-NEWLINE
 echo -e "$Y Logs stored at  $LOG_FILE"
-
+NEWLINE
 
 USERID=$(id -u)
 
