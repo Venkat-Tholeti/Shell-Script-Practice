3d100ee3b82390f0af5bcba0b8c58aa95fcd79b2
venkat <devopsaws84s@gmail.com>
2025-12-09 08:32:09 +0530

    conditions

---
+++ 09-Conditions.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# -gt ---> greater than
+# -lt ---> less than
+# -eq ---> equal 
+# -ne ---> not equal
+
+
+Number=$1
+
+if [ $Number -lt 10 ]
+then 
+    echo "Input number is less than 10"
+else
+    echo "Input number is not less than 10"
+fi
