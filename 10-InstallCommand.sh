1f963b6cd6febd603039fc3ce0da6850b82b5cf9
venkat <devopsaws84s@gmail.com>
2025-12-09 08:50:02 +0530

    install command

--- 10-InstallCommand.sh
+++ 10-InstallCommand.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+#EXIT CODES 
+#0 - SUCCESS
+#1-127 - Failures # OTHER THAN ZERO ANY EXIT STATUS IS FAILURE
+
+
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then 
    echo "ERROR: USER NEED TO SWITCH TO ROOT ACCESS"
+   exit 1 
 else
    echo "USER HAS ROOT PRIVILEGES"
 fi
