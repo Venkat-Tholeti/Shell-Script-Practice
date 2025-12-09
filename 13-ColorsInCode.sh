3f9cc5ba144a747605c77b0aaa0d4ae55333b79c
venkat <devopsaws84s@gmail.com>
2025-12-09 14:33:00 +0530

    colors

---
+++ 13-ColorsInCode.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+# Goal is to install mysql, nginx and python 3 using functions using less code.
+# Goal is to set colors for the success and failure and other conditiond.
+
+R="\e[31m"
+G="\e[32m"
+y="\e[33m"
+N="\e[0m"
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+   echo -e "$R ERROR: USER NEED TO SWITCH TO ROOT ACCESS"
+   exit 1 
+else
+   echo "USER HAS ROOT PRIVILEGES"
+fi
+
+#FUNCTION NAME WE GAVE AS VALIDATE & NEWLINE (name our choice)
+# we can provide arguments to function as we do it for script
+#VALIDATE $? MYSQL  --> here 1st argument is exit status $1 = $?, 2nd argument is what we tried to install $2 =MYSL, PYTHON3 , NGINX
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo "$2 INSTALLATION IS SUCCESS"
+    else 
+        echo "$2 INSTALLATION IS FAILURE"
+        exit 1
+    fi
+}
+
+#For gap between tasks we can use this below function or simply we can add echo or echo "" after end of task 
+NEWLINE(){
+      echo ""
+}
+
+dnf list installed mysql
+if [ $? -eq 0 ]
+then
+   echo "MYSQL PACKAGE ALREADY INSTALLED...NOTHING TO DO!"
+else 
+   echo "MYSQL is NOT INSTALLED, PROCEEDING TO INSTALLATION IN 5 SECONDS"
+   sleep 5
+   dnf install mysql -y
+   VALIDATE $? "mysql"
+fi
+NEWLINE
+dnf list installed python3
+if [ $? -eq 0 ]
+then
+   echo "PYTHON3 PACKAGE ALREADY INSTALLED...NOTHING TO DO!"
+else 
+   echo "PYTHON3 is NOT INSTALLED, PROCEEDING TO INSTALLATION IN 5 SECONDS"
+   sleep 5
+   dnf install python3 -y
+   VALIDATE $? "python3"
+fi
+NEWLINE
+dnf list installed nginx
+if [ $? -eq 0 ]
+then
+   echo "NGINX PACKAGE ALREADY INSTALLED...NOTHING TO DO!"
+else 
+   echo "NGINX is NOT INSTALLED, PROCEEDING TO INSTALLATION IN 5 SECONDS"
+   sleep 5
+   dnf install nginx -y
+   VALIDATE $? "nginx"
+fi
